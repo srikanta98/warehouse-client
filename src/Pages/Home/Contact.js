a9b29d2f2cbadca67942b9de3bf9ae21a36b39fe
srikanta98 <dattasrikanta31@gmail.com>
2022-05-26 23:40:28 +0600

    pop

--- src/Pages/Home/Contact.js
+++ src/Pages/Home/Contact.js
@@ -20,11 +20,11 @@ const Contact = () => {
           placeholder='Email Address'
           className='input w-full max-w-md'
         />
-        {/* <input
+        <input
           type='text'
           placeholder='Subject'
           className='input w-full max-w-md'
-        /> */}
+        />
         <textarea
           className='textarea w-full max-w-md'
           placeholder='Your message'
